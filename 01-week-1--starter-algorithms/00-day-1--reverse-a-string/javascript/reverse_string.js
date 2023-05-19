efe942fadae13e9d773f2a222c0148d1945f29c9
Bianca Aspin <bianca@mj94.net>
2023-05-19 13:31:22 -0700

    Solve problem.

--- 01-week-1--starter-algorithms/00-day-1--reverse-a-string/javascript/reverse_string.js
+++ 01-week-1--starter-algorithms/00-day-1--reverse-a-string/javascript/reverse_string.js
@@ -1,5 +1,20 @@
 function reverseString(str) {
-  // type your code here
+
+  // Convert the 'str' argument into an array.
+  const original = str.split('');
+
+  // Initialize empty array to add characters to on each iteration.
+  const reversed = [];
+
+  // Iterate through the array-ified string
+  original.forEach(letter => {
+
+    // On each iteration, add the letter to the beginning of the 'reversed' array
+    reversed.unshift(letter)
+  });
+
+  // Join the elements of the reversed array into a single string and return this value.
+  return reversed.join('');
 }
 
 if (require.main === module) {
@@ -11,9 +26,21 @@ if (require.main === module) {
 
   console.log("Expecting: 'ybabtac'");
   console.log("=>", reverseString("catbaby"));
+
+  console.log("")
+
+  console.log("Expecting: '!ereht olleH'");
+  console.log("=>", reverseString("Hello there!"));
 }
 
 module.exports = reverseString;
 
 // Please add your pseudocode to this file
+
+// Initialize empty array named 'reversed'
+// Split string argument into an array named 'original'
+// Iterate through string
+//  - On each iteration, remove the last letter and add it to the end of the 'reversed' array
+// Join the elements of the 'reversed' array into one string and return that string.
+
 // And a written explanation of your solution
