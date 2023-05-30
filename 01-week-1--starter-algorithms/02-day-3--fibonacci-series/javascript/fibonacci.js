1ab8e0054172a26c3b58af54667351b7734f7c3a
Bianca Aspin <bianca@mj94.net>
2023-05-30 12:19:48 -0700

    Pass tests.

--- 01-week-1--starter-algorithms/02-day-3--fibonacci-series/javascript/fibonacci.js
+++ 01-week-1--starter-algorithms/02-day-3--fibonacci-series/javascript/fibonacci.js
@@ -1,6 +1,14 @@
 function fibonacci(num) {
-  // type your code here
-}
+  const fibArray = []
+  for (let i = 0; i <= num; i++) {
+    if (i === 0 || i === 1) {
+      fibArray.push(i)
+    } else {
+      let total = fibArray[i - 1] + fibArray[i - 2]
+      fibArray.push(total)
+    }
+  } return fibArray.slice(-1).pop()
+} 
 
 if (require.main === module) {
   // add your own tests in here
@@ -21,4 +29,15 @@ if (require.main === module) {
 module.exports = fibonacci;
 
 // Please add your pseudocode to this file
+/*
+'num' represents the index of the value we are searching for in the series.
+1. Initialize empty fibonacci array.
+2. Initialize a for loop with an upper limit of num.
+  - If i is 0, just add 0 to the fibonacci array.
+  - If i is 1, add 1 to the fib array.
+  - For all other values of i, add the values at the preceding two values of i, and append to the fib array.
+3. Return the last value of the array.
+*/
+
+
 // And a written explanation of your solution
